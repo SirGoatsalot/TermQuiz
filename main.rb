683ea32d1662c0657e4d8c97159dbe2e36d295da
Foster Sullivan <fostersullivan12@gmail.com>
2026-01-22 19:20:45 -1000

    Initial Commit.

---
+++ main.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Terminal Program to create and use a quizlet equivalent
+class TermQuiz
+  def self.run
+    puts ' ==== TermQuiz ==== '
+  end
+end
+
+TermQuiz.run
